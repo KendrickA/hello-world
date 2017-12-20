ce47004f86c421c7d8dc26d1318671deb7d22c03
KendrickA <andrewkendrick37@btinternet.com>
2017-12-20 23:49:24 +0000

    kk

---
+++ test/src/main/java/test/Test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author KendrickA
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testjkkkkkll");
+
+	}
+
+}
